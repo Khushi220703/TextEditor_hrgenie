eff1b0dfb443d11f5a39d7889f542c8e75c663fc
Khushi220703 <122461997+Khushi220703@users.noreply.github.com>
2025-11-04 17:49:55 +0530

    fixed fixed db config

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,13 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => {
-    console.error('MongoDB connection error:', err);
-    process.exit(1);
-  });
-
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('MongoDB connection error:', err);
