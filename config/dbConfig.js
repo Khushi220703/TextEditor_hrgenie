dcb85d9e763b14a081b64117793986d36f7bcfb8
Khushi220703 <122461997+Khushi220703@users.noreply.github.com>
2025-11-04 17:48:43 +0530

    fixed fixed db config

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,10 +3,13 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  });
+
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('MongoDB connection error:', err);
